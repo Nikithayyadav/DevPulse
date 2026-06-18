e1668eeeba949707b3cf102c03cf4d4e1ed71f9b
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-06-18 16:32:40 +0530

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
+  migrations: { 
     path: "prisma/migrations",
   },
 }); 
